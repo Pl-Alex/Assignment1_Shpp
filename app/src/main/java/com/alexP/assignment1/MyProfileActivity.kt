1d7d05b3fd4a03ee0a8e963d650c03bbac8a4754
Stanislav Dumchykov <mechanikea@gmail.com>
2023-12-15 20:48:40 +0200

    ADD Added coroutines to project

--- app/src/main/java/com/alexP/assignment1/MyProfileActivity.kt
+++ app/src/main/java/com/alexP/assignment1/MyProfileActivity.kt
@@ -2,8 +2,13 @@ package com.alexP.assignment1
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.alexP.assignment1.databinding.MyProfileActivityBinding
 import com.bumptech.glide.Glide
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.coroutineContext
 
 class MyProfileActivity : AppCompatActivity() {
 
@@ -20,5 +25,12 @@ class MyProfileActivity : AppCompatActivity() {
         Glide.with(this)
             .load("https://unsplash.com/photos/_vnKbf9K-Vo/download?ixid=M3wxMjA3fDB8MXxhbGx8MTE4fHx8fHx8Mnx8MTcwMTgxMDA2MHw&force=true&w=640")
             .into(binding.cardViewProfileImageContainer)
+        lifecycleScope.launch {
+            test()
+        }
+    }
+
+    suspend fun test(){
+        println("ds")
     }
 }
