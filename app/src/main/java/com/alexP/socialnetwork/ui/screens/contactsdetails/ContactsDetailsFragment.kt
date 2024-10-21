34d571e840bea83169d56bc2ff7704bfcb18f227
Pl-Alex <you@example.com>
2024-10-21 17:08:12 +0200

    task-3 created contact details fragment

---
+++ app/src/main/java/com/alexP/socialnetwork/ui/screens/contactsdetails/ContactsDetailsFragment.kt
@@ -0,0 +1,17 @@
+package com.alexP.socialnetwork.ui.screens.contactsdetails
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.alexP.socialnetwork.R
+import com.alexP.socialnetwork.databinding.FragmentContactDetailsBinding
+
+class ContactsDetailsFragment : Fragment(R.layout.fragment_contact_details) {
+
+    private lateinit var binding: FragmentContactDetailsBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentContactDetailsBinding.bind(view)
+    }
+}
